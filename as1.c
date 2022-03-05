5bb58787750cd0b367d2084458ff2aa2ddaad6c9
phucndhe <100791336+phucndhe@users.noreply.github.com>
2022-03-05 14:47:43 +0700

    Update as1.c

--- as1.c
+++ as1.c
@@ -67,7 +67,8 @@ typedef struct ng
 	char email[50];
 	char working_address[100];
 	char home_address[100];
-	bd birday;
+	char birthday[10];
+	// bd birday;
 	
 }person;
 
@@ -84,17 +85,18 @@ int main()
 		scanf("%c%*c", &choice);
 
 		system("cls"); 
-
+		int i = 0;
 		printf("CONTACT KEEPER\n");
 		printf("==============\n\n");
 
 		switch(choice)
 		{
 			case '1':
-				add_contact();
+				add_contact(&i);
+				display(i);
 				break;
 			case '2':
-				edit_contact();
+				edit_contact(i);
 				break;
 			case '3':
 				delete_contact();
@@ -138,15 +140,130 @@ void print_menu()
 }
 
 
-void add_contact()
+int valid_date(int dd, int mm, int yy) {
+    if (mm < 1 || mm > 12) {
+        return 0;
+    }
+    if (dd < 1) {
+        return 0;
+    }
+
+    int days = 31;
+    if (mm == 2) {
+        days = 28;
+        if (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0)) {
+            days = 29;
+        }
+    } else if (mm == 4 || mm == 6 || mm == 9 || mm == 11) {
+        days = 30;
+    }
+
+    if (dd > days) {
+        return 0;
+    }
+    return 1;
+}
+
+void inputDate(char* d){
+	char s[100][100];
+	char date[10];
+	while(1){
+	printf("\nNhap ngay sinh (dd/MM/yyyy) : ");	
+	scanf("%s",d);
+	strcpy(date,d);
+	char* token = strtok(date,"/");
+	int i = 0;
+	while(token != NULL){
+		strcpy(s[i++],token);
+		token = strtok(NULL,"/");
+	}
+	if(i != 3){
+		printf("Khong dung format. Nhap lai!!!");
+		continue;
+	}
+	int day = atoi(s[0]);
+	int month = atoi(s[1]);
+	int year = atoi(s[2]);
+	if(valid_date(day,month,year) == 0){
+		printf("Khong hop le. Nhap lai!!");
+		continue;
+	}
+	return;
+    }
+}
+void inputPhone(char* phone){
+	while(1){
+	printf("\nNhap so dien thoai : ");	
+	scanf("%s",phone);
+	if(strlen(phone) < 9 || strlen(phone) > 10){
+		printf("\nSo dien thoai can tu 9-10 so");
+		continue;
+	}
+	int i = 0;
+	int check = 1;
+	for(i = 0;i < strlen(phone);i++){
+		if(!isdigit(phone[i])){
+			printf("\nSo dien thoai phai gom cac chu so");
+			check = 0;
+			break;
+		}
+	}
+	if(check){
+		return;
+	}	
+	}
+}
+void display(int j){
+	int i = 0;
+	for(i = 0;i < j;i++){
+		printf("%-10s | %-25s | %-20s | %-20s | %-30s | %-30s | %-10s | %-10s\n",man[i].first_name,man[i].last_name,
+		man[i].email,man[i].company,man[i].working_address,man[i].home_address,man[i].birthday,man[i].phone_number);
+	}
+}
+int getChoice(int min,int max){
+	while(1){
+		int k;
+		printf("Nhap lua chon : ");
+		scanf("%d",&k);
+		if(k >= min && k <= max){
+			return k;
+		}
+	}
+}
+person create(){
+	person p;
+	fflush(stdin);
+	printf("\n Nhap ho : ");
+	gets(p.first_name);
+	printf("\n Nhap ten : ");
+	gets(p.last_name);
+	printf("\nNhap ten cong ty : ");
+	gets(p.company);
+	inputPhone(p.phone_number);
+	fflush(stdin);
+	printf("\nNhap email : ");
+	gets(p.email);
+	printf("\nDia chi lam viec : ");
+	gets(p.working_address);
+	printf("\ndia chi nha : ");
+	gets(p.home_address);
+	inputDate(p.birthday);
+	fflush(stdin);
+	return p;
+}
+void add_contact(int *i)
 {
-	printf("Write your code here to add new contact.");
+	person p = create();
+	man[(*i)] = p;
+	(*i)++;
 }
 
 
-void edit_contact()
+void edit_contact(int j)
 {
-	printf("Write your code here to edit contact.");
+	display(j);
+	int index = getChoice(1,j) - 1;
+	man[index] = create();
 }
 
 
